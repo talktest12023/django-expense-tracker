c2b5f88de8551890006615c0719797a0c37cde14
Rahul <a.sanjayrahul@outlook.com>
2025-03-29 12:06:51 +0530

    Fixed missing static directory

--- varavuselavu/settings.py
+++ varavuselavu/settings.py
@@ -131,5 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
