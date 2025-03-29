8eb29df48fca3f5fb28a951165aa2a23f0c7241e
Rahul <a.sanjayrahul@outlook.com>
2025-03-29 15:30:39 +0530

    create super user page modified

--- selavu/create_superuser.py
+++ selavu/create_superuser.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = "rahuladmin"
+username = "rahuladmin2"
 email = "talktest12023@gmail.com"
 password = "tarento123"
 
